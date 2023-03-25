16f47badc6c6ac16acebf243ae5414678786f9e8
Christober S <christober2907@gmail.com>
2023-03-25 21:48:07 +0530

    Varible declaration

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main //every statement in go written in packages
 import "fmt" //In go if we need do use built in packages we need to import explicityly
 
 func main() { // program starts form main
-	fmt.Println("Hello World!")
+	fmt.Println("Welcome to the Emirates Airline")
+	fmt.Println("Book your tickets here!")
+
+	var airlineName = "Emirates Airline" // Variable declaration
+	fmt.Print(airlineName)
 }
