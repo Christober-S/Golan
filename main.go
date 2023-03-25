1720ebaa88924c194329a1a7a45deb9a57342259
Christober S <christober2907@gmail.com>
2023-03-25 21:59:07 +0530

    Constatnt Varible declaration

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main //every statement in go written in packages
 import "fmt" //In go if we need do use built in packages we need to import explicityly
 
 func main() { // program starts form main
-	fmt.Println("Welcome to the Emirates Airline")
-	fmt.Println("Book your tickets here!")
 
 	var airlineName = "Emirates Airline" // Variable declaration
-	fmt.Print(airlineName)
+	const airlineTickets = 200           // constant values cannot be changed
+	var remainingTickets = 200
+
+	fmt.Println("Welcome to the", airlineName, "ticket booking application")
+	fmt.Println("We have total of", airlineTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Book your tickets here!")
+
 }
