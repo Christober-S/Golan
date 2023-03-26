bd069a7c719fae87f42a45e2c09608494fe7a935
Christober S <christober2907@gmail.com>
2023-03-26 08:06:09 +0530

    String & int datatypes Go

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ func main() { // program starts form main
 	var airlineName = "Emirates Airline" // Variable declaration
 	const airlineTickets = 200           // constant values cannot be changed
 	var remainingTickets = 200
+	var userName string // To specifiy the type of data
+	var userTicket int  // To specifiy the int type of dT
 
-	fmt.Printf("Welcome to the %v ticket booking application", airlineName)
+	userName = "christober"
+	userTicket = 7
+
+	fmt.Printf("Welcome to the %v ticket booking application", airlineName) // %v is  the placehoder for value
 	fmt.Println("We have total of", airlineTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Book your tickets here!")
+	fmt.Printf("The user %v has booked %v number of ticktets", userName, userTicket)
 
 }
