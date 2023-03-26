10cb3a892a01ba4294881c00bec832f31a04a821
Christober S <christober2907@gmail.com>
2023-03-26 12:03:15 +0530

    Syntatic Sugar concept

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main //every statement in go written in packages
 
-//In go if we need do use built in packages we need to import explicityly
+import "fmt" //In go if we need do use built in packages we need to import explicityly
 
 func main() { // program starts form main
 
-	/*
-			#DATA TYPES...
-
-			GO			JAVA
-			int8		byte
-			int16		short
-		    int32		int
-			int 64		long
-			uint		postive,whole numbers
-
-
-			float32
-			float64
-			complex32
-			complex64
-
-	*/
+	airlineName := "Emirates" //This is same as var airlineName = "Emirates"
+	//But it simplifies that and it doesn't add new functionality
+	fmt.Print(airlineName)
 
 }
