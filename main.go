788617b6f06d84b2841cee20e9e21bfe88b528ea
Christober S <christober2907@gmail.com>
2023-03-26 15:46:04 +0530

    Firstname, lastname, email getted as input

--- main.go
+++ main.go
@@ -4,18 +4,27 @@ func main() { // program starts form main
 	var airlineName = "Emirates Airline" // Variable declaration
 	const airlineTickets = 200           // constant values cannot be changed
 	var remainingTickets = 200
-	var userName string // To specifiy the type of data
-	var userTicket int  // To specifiy the int type of dT
 
-	fmt.Printf("Welcome to the %v ticket booking application", airlineName)
-
-	fmt.Printf("Welcome to the %v ticket booking application", airlineName) // %v is  the placehoder for value
+	fmt.Printf("Welcome to the %v ticket booking application\n", airlineName) // %v is  the placehoder for value
 	fmt.Println("We have total of", airlineTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Book your tickets here!")
-	fmt.Print("Enter the user name: ")
-	fmt.Scan(&userName)
-	fmt.Print("Enter the no of tickets: ")
-	fmt.Scan(&userTicket)
-	fmt.Printf("The user %v has booked %v number of ticktets", userName, userTicket)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter your no of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
