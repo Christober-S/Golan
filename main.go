1d35dc20a48c3f595f4e355275ed5c1134180fce
Christober S <christober2907@gmail.com>
2023-03-30 18:09:58 +0530

    Example for the infinte for loop

--- main.go
+++ main.go
@@ -8,31 +8,34 @@ func main() { // program starts form main
 	fmt.Printf("Welcome to the %v ticket booking application\n", airlineName) // %v is  the placehoder for value
 	fmt.Println("We have total of", airlineTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Book your tickets here!")
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	var bookings []string //Array of size 50 with String datatype
+	for { //infinte for loop
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		var bookings []string //Array Slice using this we don't need to specifiy the data type
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Print("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Enter your no of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter your email address: ")
+		fmt.Scan(&email)
 
-	bookings = append(bookings, firstName+" "+lastName)
-	remainingTickets = remainingTickets - userTickets //In go lang we can't do calculation's if both are not same datatypes
+		fmt.Print("Enter your no of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets available for %v\n", remainingTickets, airlineName)
-	fmt.Printf("%v\n", bookings[0])
-	fmt.Printf("%T\n", bookings)
-	fmt.Printf("Length of array:%v", len(bookings)) // length of array
+		bookings = append(bookings, firstName+" "+lastName) //In slice we can add element using append() method
+		remainingTickets = remainingTickets - userTickets   //In go lang we can't do calculation's if both are not same datatypes
 
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets available for %v\n", remainingTickets, airlineName)
+		fmt.Printf("%v\n", bookings[0])
+		fmt.Printf("%T\n", bookings)
+		fmt.Printf("Length of array:%v\n", len(bookings)) // length of array
+
+	}
 }
